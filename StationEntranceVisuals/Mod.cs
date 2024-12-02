8ee58ca7a460997e1cf90298107fa19c7d2b1eb3
rodrigmatrix <rodrigmatrix@gmail.com>
2024-12-01 22:08:11 -0300

    fixing initialization issues

--- StationEntranceVisuals/Mod.cs
+++ StationEntranceVisuals/Mod.cs
@@ -6,7 +6,9 @@ using HarmonyLib;
 using StationEntranceVisuals.BridgeWE;
 using StationEntranceVisuals.Utils;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -14,56 +16,78 @@ using System.Reflection;
 namespace StationEntranceVisuals
 {
     public class Mod : IMod
-    {
-        public static ILog log = LogManager.GetLogger($"{nameof(StationEntranceVisuals)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+ {
+     public static ILog log = LogManager.GetLogger($"{nameof(StationEntranceVisuals)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
 
-        public void OnLoad(UpdateSystem updateSystem)
-        {
-            log.Info(nameof(OnLoad));
+     public void OnLoad(UpdateSystem updateSystem)
+     {
+         log.Info(nameof(OnLoad));
 
-            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
-                log.Info($"Current mod asset at {asset.path}");
+         if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+             log.Info($"Current mod asset at {asset.path}");
+         
+         var bw = new BackgroundWorker();
+         bw.DoWork += DeleteOldFiles;
+         bw.RunWorkerAsync();
+         GameManager.instance.onGameLoadingComplete += DoWhenLoaded;
+     }
+     
+     private static void DeleteOldFiles(object sender, DoWorkEventArgs e)
+     {
+         FileUtils.DeleteOldFiles();
+     }  
 
-            DoPatches();
-            RegisterFilesToWE();
-        }
+     private void DoWhenLoaded(Colossal.Serialization.Entities.Purpose purpose, GameMode mode)
+     {
+         log.Info($"Loading patches");
+         DoPatches();
+         RegisterFilesToWe();
+         GameManager.instance.onGameLoadingComplete -= DoWhenLoaded;
+     }
 
-        private void RegisterFilesToWE()
-        {
-            string modPath = Path.GetDirectoryName(GameManager.instance.modManager.First(x => x.asset.assembly == typeof(Mod).Assembly).asset.path);
-            var localImagesDirectory = Path.Combine(modPath, "weImageAtlases");
-            var localLayoutsDirectory = Path.Combine(modPath, "weLayouts");
-            WEImageManagementBridge.RegisterImageAtlas(typeof(Mod).Assembly, "main", Directory.GetFiles(localImagesDirectory));
-            WETemplatesManagementBridge.RegisterCustomTemplates(typeof(Mod).Assembly, localLayoutsDirectory);
-            WETemplatesManagementBridge.RegisterLoadableTemplatesFolder(typeof(Mod).Assembly, localLayoutsDirectory);
-        }
+     private static void RegisterFilesToWe()
+     {
+         string modPath = Path.GetDirectoryName(GameManager.instance.modManager.First(x => x.asset.assembly == typeof(Mod).Assembly).asset.path);
+         var localImagesDirectory = Path.Combine(modPath, "weImageAtlases");
+         var localLayoutsDirectory = Path.Combine(modPath, "weLayouts");
+         WEImageManagementBridge.RegisterImageAtlas(typeof(Mod).Assembly, "main", Directory.GetFiles(localImagesDirectory));
+         WETemplatesManagementBridge.RegisterCustomTemplates(typeof(Mod).Assembly, localLayoutsDirectory);
+         WETemplatesManagementBridge.RegisterLoadableTemplatesFolder(typeof(Mod).Assembly, localLayoutsDirectory);
+     }
 
-        private void DoPatches()
-        {
-            if (AppDomain.CurrentDomain.GetAssemblies().SingleOrDefault(assembly => assembly.GetName().Name == "BelzontWE") is Assembly weAssembly)
-            {
-                var exportedTypes = weAssembly.ExportedTypes;
-                foreach (var (type, sourceClassName) in new List<(Type, string)>() {
-                    (typeof(WEFontManagementBridge), "FontManagementBridge"),
-                    (typeof(WEImageManagementBridge), "ImageManagementBridge"),
-                    (typeof(WETemplatesManagementBridge), "TemplatesManagementBridge"),
-                })
-                {
-                    var targetType = exportedTypes.First(x => x.Name == sourceClassName);
-                    foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.Static))
-                    {
-                        var srcMethod = targetType.GetMethod(method.Name, ReflectionUtils.allFlags, null, method.GetParameters().Select(x => x.ParameterType).ToArray(), null);
-                        if (srcMethod != null) Harmony.ReversePatch(srcMethod, method);
-                        else log.Warn($"Method not found while patching WE: {targetType.FullName} {srcMethod.Name}({string.Join(", ", method.GetParameters().Select(x => $"{x.ParameterType}"))})");
-                    }
-                }
-            }
-        }
+     private void DoPatches()
+     {
+         if (AppDomain.CurrentDomain.GetAssemblies().SingleOrDefault(assembly => assembly.GetName().Name == "BelzontWE") is Assembly weAssembly)
+         {
+             var exportedTypes = weAssembly.ExportedTypes;
+             foreach (var (type, sourceClassName) in new List<(Type, string)>() {
+                 (typeof(WEFontManagementBridge), "FontManagementBridge"),
+                 (typeof(WEImageManagementBridge), "ImageManagementBridge"),
+                 (typeof(WETemplatesManagementBridge), "TemplatesManagementBridge"),
+             })
+             {
+                 var targetType = exportedTypes.First(x => x.Name == sourceClassName);
+                 foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.Static))
+                 {
+                     var srcMethod = targetType.GetMethod(method.Name, ReflectionUtils.allFlags, null, method.GetParameters().Select(x => x.ParameterType).ToArray(), null);
+                     if (srcMethod != null)
+                     {
+                         Harmony.ReversePatch(srcMethod, method);
+                     }
+                     else log.Warn($"Method not found while patching WE: {targetType.FullName} {srcMethod.Name}({string.Join(", ", method.GetParameters().Select(x => $"{x.ParameterType}"))})");
+                 }
+             }
+         }
+         else
+         {
+             throw new Exception("Write Everywhere dll file required for using this mod! Check if it's enabled.");
+         }
+     }
 
-        public void OnDispose()
-        {
-            log.Info(nameof(OnDispose));
-        }
+     public void OnDispose()
+     {
+         log.Info(nameof(OnDispose));
+     }
 
-    }
+ }
 }
---
+++ StationEntranceVisuals/Utils/FileUtils.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Colossal.PSI.Environment;
+
+namespace StationEntranceVisuals.Utils;
+
+public abstract class FileUtils
+{
+     
+     private static readonly string WePath = Path.Combine(
+          EnvPath.kUserDataPath,
+          "ModsData",
+          "Klyte45Mods",
+          "WriteEverywhere"
+     );
+     
+     private static readonly string WeImagesDirectory = Path.Combine(
+          WePath,
+          "imageAtlases",
+          "_StationEntranceVisuals"
+     );
+     
+     private static readonly string WeLayoutsDirectory = Path.Combine(
+          WePath,
+          "layouts",
+          "_StationEntranceVisuals"
+     );
+     
+     private static readonly string OldWeImagesDirectory = Path.Combine(
+          WePath,
+          "imageAtlases",
+          "StationEntranceVisuals"
+     );
+     
+     private static readonly string OldWeLayoutsDirectory = Path.Combine(
+          WePath,
+          "layouts",
+          "StationEntranceVisuals"
+     );
+     
+     public static void DeleteOldFiles()
+     {
+          try
+          {
+               DeleteIfExists(OldWeImagesDirectory);
+               DeleteIfExists(OldWeLayoutsDirectory);
+               DeleteIfExists(WeImagesDirectory);
+               DeleteIfExists(WeLayoutsDirectory);
+          }
+          catch (Exception e)
+          {
+               Mod.log.Info(e.Message);
+          }
+     }
+     
+     private static void DeleteIfExists(string path)
+     {
+          var folderExists = Directory.Exists(path);
+          if (folderExists)
+               Directory.Delete(path, true);
+     }
+}
